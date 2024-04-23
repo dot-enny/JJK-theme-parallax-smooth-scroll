ecbb613882a2588b0a1cf6d7c1936ba843e61fea
dot-enny <olaniyaneniola0225@gmail.com>
2024-04-23 10:07:59 +0100

    feat: added preloader component.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,20 @@ import Gojo from "/img/jjk-gojo.jpg";
 import Megumi from "/img/jjk-megumi.jpg";
 import Yuji from "/img/jjk-yuji.jpg";
 import Nobara from "/img/jjk-nobara.jpg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Lenis from '@studio-freight/lenis'
+import { Preloader } from "./components/Preloader";
+import { AnimatePresence } from "framer-motion";
 
 export default function App () {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   useEffect(() => {
     const lenis = new Lenis()
@@ -19,33 +29,39 @@ export default function App () {
   }, []);
 
   return (
-    <main className="min-h-screen text-white">
-      <Hero />
-      <Section 
-        image={Gojo} 
-        tag="Gojo Satorou" 
-        title="The Strongest Jujutsu Sorcerer"
-        description="Gojo Satorou is the strongest jujutsu sorcerer in the world. He is a teacher at Tokyo Metropolitan Curse Technical College and is known for his limitless cursed energy and Six Eyes technique."
-      />
-      <Section
-        image={Megumi}
-        tag="Fushiguro Megumi"
-        title="The Heir of the Zenin Clan"
-        description="Fushiguro Megumi is a student at Tokyo Metropolitan Curse Technical College. He is a descendant of the Zenin clan and is known for his ability to summon Shikigami."
-      />
-      <Section 
-        image={Yuji}
-        tag="Itadori Yuji"
-        title="The Host of Sukuna"
-        description="Itadori Yuji is a student at Tokyo Metropolitan Curse Technical College. He is the host of Ryomen Sukuna, the King of Curses, and is known for his immense physical strength."
-      />
-      <Section 
-        image={Nobara}
-        tag="Kugisaki Nobara"
-        title="The Exterminator"
-        description="Kugisaki Nobara is a student at Tokyo Metropolitan Curse Technical College. She is known for her ability to control nails and is a skilled exorcist."
-      />
-    </main>
+    <>
+      <AnimatePresence mode="wait">
+        { isLoading && <Preloader /> }
+      </AnimatePresence>
+        
+      <main className="min-h-screen text-white">
+        <Hero />
+        <Section 
+          image={Gojo} 
+          tag="Gojo Satorou" 
+          title="The Strongest Jujutsu Sorcerer"
+          description="Gojo Satorou is the strongest jujutsu sorcerer in the world. He is a teacher at Tokyo Metropolitan Curse Technical College and is known for his limitless cursed energy and Six Eyes technique."
+        />
+        <Section
+          image={Megumi}
+          tag="Fushiguro Megumi"
+          title="The Heir of the Zenin Clan"
+          description="Fushiguro Megumi is a student at Tokyo Metropolitan Curse Technical College. He is a descendant of the Zenin clan and is known for his ability to summon Shikigami."
+        />
+        <Section 
+          image={Yuji}
+          tag="Itadori Yuji"
+          title="The Host of Sukuna"
+          description="Itadori Yuji is a student at Tokyo Metropolitan Curse Technical College. He is the host of Ryomen Sukuna, the King of Curses, and is known for his immense physical strength."
+        />
+        <Section 
+          image={Nobara}
+          tag="Kugisaki Nobara"
+          title="The Exterminator"
+          description="Kugisaki Nobara is a student at Tokyo Metropolitan Curse Technical College. She is known for her ability to control nails and is a skilled exorcist."
+        />
+      </main>
+    </>
   )
 };
  
---
+++ src/components/Preloader.tsx
@@ -0,0 +1,34 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { easings } from "../utils/animations";
+import Logo from "../../public/jjk-logo.svg";
+import { useState } from "react";
+
+export const Preloader = () => {
+
+  const PreloaderVariants = {
+    exit: { y: "-100%", transition: { duration: 1.5, delay: 0.2, ease: easings.easeOutQuart } }
+  };
+
+  const logoVariants = {
+    initial: { opacity: 0, scale: 0.3 },
+    animate: { 
+      opacity: 1, 
+      scale: 0.5,
+      transition: { duration: 2, ease: easings.easeInOutQuint }
+    },
+    exit: { opacity: 0, transition: { duration: 0.2, ease: easings.easeInOutQuint } }
+  };
+
+  const [isLogoVisible, setIsLogoVisible] = useState(true);
+
+  return (
+    <motion.div className="fixed inset-0 z-50 bg-white flex justify-center items-center" variants={PreloaderVariants} exit="exit">
+      <AnimatePresence mode="wait">
+        {
+          isLogoVisible &&
+          <motion.img src={Logo} alt="JUJTSU KAISEN" variants={logoVariants} initial="initial" animate="animate" exit="exit"  onAnimationComplete={() => setIsLogoVisible(false)} />
+        }
+      </AnimatePresence>
+    </motion.div>
+  )
+}
