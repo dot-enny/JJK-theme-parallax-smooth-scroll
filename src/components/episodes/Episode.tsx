9a8e33fd848d4e5a1de3df8ce167371135542c48
dot-enny <olaniyaneniola0225@gmail.com>
2024-04-26 17:34:55 +0100

    feat: fetched and displayed episodes data on episodes page.

---
+++ src/components/episodes/Episode.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://api.jikan.moe/v4";
+
+export default function Episode({ episodeId, image }: { episodeId: number, image: string }) {
+    const [episodeData, setEpisodeData] = useState<any>(null);
+
+    useEffect(() => {
+        fetchEpisodeData();
+    }, []);
+
+    const fetchEpisodeData = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}/anime/40748/episodes/${episodeId}`);
+            const data = await response.json();
+            setEpisodeData(data.data);
+        } catch (error) {
+            console.error("Error fetching anime data:", error);
+        }
+    };
+
+    return (
+        <>
+            {episodeData && (
+                <div>
+                    <div className="p-4 h-[50vh] min-h-screen px-20 py-24 grid grid-cols- gap-12 items-center">
+                        { episodeData.mal_id && <img src={image} alt="episode image" className="justify-self-center" /> }
+                        <div className="rounded-lg">
+                            <div className="flex justify-between text-lg font-semibold mb-9">
+                                <span>{episodeData.mal_id}</span>
+                                <span>{episodeData.title}</span>
+                            </div>
+                            <p className="">{episodeData.synopsis.slice(0, 300)}...</p>
+                            <span 
+                                className="underline text-gray-400 cursor-pointer"
+                                onClick={() => setEpisodeDetails(true)}
+                            >
+                                see details
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,7 +1,48 @@
+import React, { useEffect, useState } from "react";
+import Episode from "../components/episodes/Episode";
+
+const BASE_URL = "https://api.jikan.moe/v4";
+
 export default function Episodes () {
-   
+    const [animeData, setAnimeData] = useState<any>(null);
+    const [animeVideos, setAnimeVideos] = useState<any>(null);
+
+    useEffect(() => {
+        fetchAnimeData();
+        fetchAnimeVideosEpisodes();
+    }, []);
+
+    const fetchAnimeData = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}/anime/40748/episodes`);
+            const data = await response.json();
+            setAnimeData(data.data);
+        } catch (error) {
+            console.error("Error fetching anime data:", error);
+        }
+    };
+
+    const fetchAnimeVideosEpisodes = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}/anime/40748/videos/episodes`);
+            const data = await response.json();
+            setAnimeVideos(data.data);
+        } catch (error) {
+            console.error("Error fetching anime data:", error);
+        }
+    };
+
     return (
-        <>
-        </>
+        <div className="bg-black">
+            {animeData && (
+                <div className="grid lg:grid-cols-2">
+                    {animeData.map((episode: any) => (
+                        <React.Fragment key={episode.mal_id}>
+                            <Episode episodeId={episode.mal_id} image={animeVideos[episode.mal_id]?.images.jpg.image_url} />
+                        </React.Fragment>
+                    ))}
+                </div>
+            )}
+        </div>
     );
 }
