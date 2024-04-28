c57f65579fb529d5954545e925cf27eb6c3c5cd8
dot-enny <olaniyaneniola0225@gmail.com>
2024-04-28 19:24:18 +0100

    refactor: moved episode details component and hide episode component into separate files.

--- src/components/episodes/Episode.tsx
+++ src/components/episodes/Episode.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
+import { EpisodeDetails } from "./episode-details/EpisodeDetails"
 
 const BASE_URL = "https://api.jikan.moe/v4";
 
-export default function Episode({ episodeId, image }: { episodeId: number, image: string }) {
+export default function Episode({ episodeId, image } : { episodeId: number, image: string }) {
     const [episodeData, setEpisodeData] = useState<any>(null);
-    const [episodeDetails, setEpisodeDetails] = useState<any>(null);
+    const [episodeDetails, setEpisodeDetails] = useState<boolean>(false);
 
-    const toggleEpisodeDetails = () => {
-        setEpisodeDetails(!episodeDetails); // Toggle state on click
-    };
+   const toggleDetails = () => {
+    setEpisodeDetails(!episodeDetails);
+   };
 
     useEffect(() => {
         fetchEpisodeData();
@@ -29,9 +30,10 @@ export default function Episode({ episodeId, image }: { episodeId: number, image
         <>
             {episodeData && (
                 <>
-                    <div 
-                        className="m-auto grid cursor-pointer"
-                        onClick={toggleEpisodeDetails}
+                    {/* episode card */}
+                    <button
+                        className="m-auto"
+                        onClick={toggleDetails}
                     >
                         {episodeData.mal_id && <img src={image} alt="episode image" />}
                         <div className="flex justify-start text-lg font-semibold cursor-pointer mt-5">
@@ -40,65 +42,15 @@ export default function Episode({ episodeId, image }: { episodeId: number, image
                             <span>{episodeData.title.slice(0, 20)}</span>
                         </div>
 
-                        {episodeDetails && 
-                            <EpisodeDetails episode={{image, ...episodeData}} toggleDetails={toggleEpisodeDetails} />}
-                    </div>
+                    </button>
+
+                    {/* episode details */}
+                    {episodeDetails && 
+                        <EpisodeDetails episode={{image, ...episodeData}} toggleDetails={toggleDetails} />}
                 </>
             )}
         </>
     );
 }
 
-interface EpisodeProps {
-    episode: any;
-    toggleDetails: (isMenuOpen: boolean) => void;
-}
 
-const EpisodeDetails = ({episode, toggleDetails}: EpisodeProps) => {
-    return (
-        <div className="bg-gradient-to-b from-purple-950 via-pink-950 to-black fixed inset-20 rounded cursor-pointer">
-            <div 
-                className="text-3xl absolute top-0 right-0"
-                onClick={() => toggleDetails(false)}
-            >
-                    x
-            </div>
-            <div className="w-3/4 mx-auto pt-10">
-                <div className="grid grid-cols-2">
-                    <div className="font-semibold">
-                        <div className="grid grid-cols-2">
-                            <span>Episode:</span>
-                            <span>{episode.mal_id}</span>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <span>Duration:</span>
-                            <span>{Math.round(episode.duration / 60)} minutes</span>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <span>English Title:</span>
-                            <span>{episode.title}</span>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <span>Japanese Title:</span>
-                            <span>{episode.title_japanese}</span>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <span>Romanji Title:</span>
-                            <span>{episode.title_romanji}</span>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <span>Aired:</span>
-                            <span>{episode.aired}</span>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <span>MAL Reference:</span>
-                            <a href={episode.url} target="blank">{episode.url.slice(0, 40)}...</a>
-                        </div>
-                    </div>
-                    <img src={episode.image} alt="episode image" className="ml-auto" />
-                </div>
-                <p className="mt-32">{episode.synopsis}</p>
-            </div>
-        </div>
-    )
-};
---
+++ src/components/episodes/episode-details/EpisodeDetails.tsx
@@ -0,0 +1,50 @@
+import { HideEpisodeDetails } from "./HideEpisodeDetails";
+
+interface EpisodeProps {
+    episode: any;
+    toggleDetails: () => void;
+}
+
+export const EpisodeDetails = ({episode, toggleDetails}: EpisodeProps) => {
+    return (
+        <div className="z-20 bg-gradient-to-b from-purple-950 via-pink-950 to-black fixed inset-20 rounded pointer-events-none">
+            <HideEpisodeDetails toggleDetails={toggleDetails} />
+            <div className="w-3/4 mx-auto pt-10">
+                <div className="grid grid-cols-2">
+                    <div className="font-semibold">
+                        <div className="grid grid-cols-2">
+                            <span>Episode:</span>
+                            <span>{episode.mal_id}</span>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>Duration:</span>
+                            <span>{Math.round(episode.duration / 60)} minutes</span>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>English Title:</span>
+                            <span>{episode.title}</span>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>Japanese Title:</span>
+                            <span>{episode.title_japanese}</span>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>Romanji Title:</span>
+                            <span>{episode.title_romanji}</span>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>Aired:</span>
+                            <span>{episode.aired}</span>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>MAL Reference:</span>
+                            <a href={episode.url} target="blank">{episode.url.slice(0, 40)}...</a>
+                        </div>
+                    </div>
+                    <img src={episode.image} alt="episode image" />
+                </div>
+                <p className="mt-32">{episode.synopsis}</p>
+            </div>
+        </div>
+    )
+};
