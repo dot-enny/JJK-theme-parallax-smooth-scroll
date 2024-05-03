23ca5ad779ae16ee514e59ce6d3640f4dfdf3e1f
dot-enny <olaniyaneniola0225@gmail.com>
2024-05-03 10:54:42 +0100

    feat: episode loading state.

--- src/components/episodes/Episode.tsx
+++ src/components/episodes/Episode.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import { EpisodeDetails } from "./episode-details/EpisodeDetails"
+import { AnimatePresence } from "framer-motion";
 
 const BASE_URL = "https://api.jikan.moe/v4";
 
-export default function Episode({ episodeId, image, setPointerEvents } : { episodeId: number, image: string, setPointerEvents: () => void }) {
+export default function Episode({ episodeId, image, setPointerEvents }: { episodeId: number, image: string, setPointerEvents: () => void }) {
     const [episodeData, setEpisodeData] = useState<any>(null);
     const [episodeDetails, setEpisodeDetails] = useState<boolean>(false);
 
-   const toggleDetails = () => {
-    setEpisodeDetails(!episodeDetails);
-    setPointerEvents();
-   };
+    const toggleDetails = () => {
+        setEpisodeDetails(!episodeDetails);
+        setPointerEvents();
+    };
 
     useEffect(() => {
         fetchEpisodeData();
@@ -29,23 +30,33 @@ export default function Episode({ episodeId, image, setPointerEvents } : { episo
 
     return (
         <>
-            {episodeData && (
-                <>
-                    {/* episode card */}
-                    <button
-                        className="m-auto hover:scale-105 transition-transform duration-300 ease-in-out"
-                        onClick={toggleDetails}
-                    >
+            {/* episode card */}
+            <button
+                className="m-auto hover:scale-105 transition-transform duration-300 ease-in-out"
+                onClick={toggleDetails}
+            >
+                {episodeData ? (
+                    <>
                         {episodeData.mal_id && <img src={image} alt="episode image" className="w-[200px] h-[113px]" />}
                         <div className="flex justify-start text-lg font-semibold cursor-pointer mt-5">
                             <span>{episodeData.mal_id}</span>
                             <span className="mr-2">.</span>
                             <span>{episodeData.title.slice(0, 20)}</span>
                         </div>
+                    </>
+                ) : (
+                    <div>
+                        <div className="w-[200px] h-[113px] bg-gradient-to-b from-slate-900 via-gray-700 to-slate-900 animate-pulse" />
+                        <div className="flex justify-start text-lg font-semibold cursor-pointer mt-5">
+                            <span>{episodeId}</span>
+                            <span className="mr-2">.</span>
+                            <span className="w-full bg-gradient-to-b from-slate-900 via-gray-700 to-slate-900 animate-pulse"></span>
+                        </div>
+                    </div>
+                )}
+            </button>
 
-                    </button>
-
-                    {/* episode details */}
+            {/* episode details */}
                     {episodeDetails && 
                         <EpisodeDetails episode={{image, ...episodeData}} toggleDetails={toggleDetails} />}
                 </>
