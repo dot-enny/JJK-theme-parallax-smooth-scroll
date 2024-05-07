6fec07987a0e35c068608e8bb5c4fc10804b5be5
dot-enny <olaniyaneniola0225@gmail.com>
2024-05-07 19:13:13 +0100

    refactor: used custom hooks to fetch anime data and updated process for setting anime season ID to avoid bugs.

--- src/components/episodes/Seasons.tsx
+++ src/components/episodes/Seasons.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 const BASE_URL = "https://api.jikan.moe/v4";
 
 interface SeasonProps {
-    setSeason: (seasonNumber: number) => void;
+    setSeasonId: (seasonNumber: number) => void;
 }
 
-export const Seasons = ({ setSeason } : SeasonProps) => {
+export const Seasons = ({ setSeasonId } : SeasonProps) => {
 
     const [season1Data ,setSeason1Data] = useState<any>();
     const [season2Data ,setSeason2Data] = useState<any>();
@@ -39,7 +39,7 @@ export const Seasons = ({ setSeason } : SeasonProps) => {
         <div className="bg-black h-screen p-20 grid grid-cols-2 gap- gap-44">
             {
                 season1Data ? (
-                    <button className="bg-gray-500 h-[60%] group" onClick={() => setSeason(1)}>
+                    <button className="bg-gray-500 h-[60%] group" onClick={() => setSeasonId(40748)}>
                         <img src={season1Data.images.webp.large_image_url} alt="season 1 thumbnail" className="w-full h-full group-hover:scale-105 transition-transform duration-300 ease-in-out" />
                         <h1 className="text-xl font-serif mt-10">Season 1</h1>
                     </button>
@@ -53,7 +53,7 @@ export const Seasons = ({ setSeason } : SeasonProps) => {
 
             {
                 season2Data ? (
-                    <button className="bg-gray-500 h-[60%] group" onClick={() => setSeason(2)}>
+                    <button className="bg-gray-500 h-[60%] group" onClick={() => setSeasonId(51009)}>
                         <img src={season2Data.images.webp.large_image_url} alt="season 2 thumbnail" className="w-full h-full group-hover:scale-105 transition-transform duration-300 ease-in-out" />
                         <h1 className="text-xl font-serif mt-10">Season 2</h1>
                     </button>
--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,44 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Episode from "../components/episodes/Episode";
 import { motion } from "framer-motion";
 import { Seasons } from "../components/episodes/Seasons";
-
-const BASE_URL = "https://api.jikan.moe/v4";
+import { useFetchAnimeData } from "../hooks/useFetchAnimeData";
+import { useFetchAnimeVideosEpisodes } from "../hooks/useFetchAnimeVideosEpisodes";
 
 export default function Episodes () {
-    const [animeData, setAnimeData] = useState<any>(null);
-    const [animeVideos, setAnimeVideos] = useState<any>(null);
     const [pointerEvents, setPointerEvents] = useState<boolean>(false);
-    const [season, setSeason] = useState<number>(0);
-    const seasonId = season === 1 ? 40748 : 51009;
+    const [seasonId, setSeasonId] = useState<number>(0);
+    const animeData = useFetchAnimeData(seasonId);
+    const animeEpisodeThumbNails = useFetchAnimeVideosEpisodes(seasonId);
 
     const togglePointerEvents = () => {
         setPointerEvents(!pointerEvents);
-    }
-
-    useEffect(() => {
-        fetchAnimeData();
-        fetchAnimeVideosEpisodes();
-    }, [season]);
-
-    const fetchAnimeData = async () => {
-        try {
-            const response = await fetch(`${BASE_URL}/anime/${seasonId}/episodes`);
-            const data = await response.json();
-            setAnimeData(data.data);
-        } catch (error) {
-            console.error("Error fetching anime data:", error);
-        }
-    };
-
-    const fetchAnimeVideosEpisodes = async () => {
-        try {
-            const response = await fetch(`${BASE_URL}/anime/${seasonId}/videos/episodes`);
-            const data = await response.json();
-            setAnimeVideos(data.data);
-        } catch (error) {
-            console.error("Error fetching anime data:", error);
-        }
     };
 
     const EpisodeGridVariants = {
@@ -68,8 +42,8 @@ export default function Episodes () {
 
     return (
         <>
-            {season === 0 ? (
-                <Seasons setSeason={setSeason} />
+            {seasonId === 0 ? (
+                <Seasons setSeasonId={setSeasonId} />
             ) : (                
                 <div className={`bg-black min-h-screen ${pointerEvents ? 'pointer-events-none' : 'pointer-events-auto'}`}>
                     {animeData && (
@@ -83,7 +57,7 @@ export default function Episodes () {
                                     key={episode.mal_id}
                                     variants={EpisodeVariants}
                                 >
-                                    <Episode episodeId={episode.mal_id} image={animeVideos[episode.mal_id]?.images.jpg.image_url} setPointerEvents={togglePointerEvents} seasonId={seasonId} />
+                                    <Episode episodeId={episode.mal_id} image={animeEpisodeThumbNails[episode.mal_id]?.images.jpg.image_url} setPointerEvents={togglePointerEvents} seasonId={seasonId} />
                                 </motion.div>
                             ))}
                         </motion.div>
