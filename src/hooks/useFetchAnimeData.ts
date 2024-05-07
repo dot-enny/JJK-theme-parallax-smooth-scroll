67af684114cbed4ad8367bceb865508317970547
dot-enny <olaniyaneniola0225@gmail.com>
2024-05-07 19:11:07 +0100

    refactor: moved code for fetching animeData and thumbnails on episodes page into custom hooks inside hooks folder.

---
+++ src/hooks/useFetchAnimeData.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://api.jikan.moe/v4";
+
+export function useFetchAnimeData(seasonId: number) {
+    const [animeData, setAnimeData] = useState<any>(null);
+    
+    useEffect(() => {
+        fetchAnimeData();
+    }, [seasonId]);
+
+    const fetchAnimeData = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}/anime/${seasonId}/episodes`);
+            const data = await response.json();
+            setAnimeData(data.data);
+        } catch (error) {
+            console.error("Error fetching anime data:", error);
+        }
+    };
+
+    return animeData;
+};
---
+++ src/hooks/useFetchAnimeVideosEpisodes.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://api.jikan.moe/v4";
+
+export function useFetchAnimeVideosEpisodes(seasonId: number) {
+    const [animeVideos, setAnimeVideos] = useState<any>(null);
+
+    useEffect(() => {
+        fetchAnimeVideosEpisodes();
+    }, [seasonId]);
+
+    const fetchAnimeVideosEpisodes = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}/anime/${seasonId}/videos/episodes`);
+            const data = await response.json();
+            setAnimeVideos(data.data);
+        } catch (error) {
+            console.error("Error fetching anime data:", error);
+        }
+    };
+
+    return animeVideos;
+};
