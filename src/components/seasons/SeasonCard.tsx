7f1ed75ebef1e425a15df29250338e4196ab4c32
dot-enny <olaniyaneniola0225@gmail.com>
2024-05-10 23:12:17 +0100

    feat: added fade in animation for season cards.

--- src/components/seasons/SeasonCard.tsx
+++ src/components/seasons/SeasonCard.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { SeasonCardVariant } from "../../utils/animations/variants/Season";
+
 interface SeasonCardProps {
   seasonData: any;
   setSeasonId: (seasonNumber: number) => void;
@@ -11,10 +14,13 @@ export const SeasonCard = ({ seasonData, setSeasonId }: SeasonCardProps) => {
     <>
       {
         seasonData ? (
-          <button className="bg-gray-500 h-[60%] group" onClick={() => setSeasonId(seasonData.mal_id)}>
+          <motion.button 
+            className="bg-gray-500 h-[60%] group" onClick={() => setSeasonId(seasonData.mal_id)}
+            variants={SeasonCardVariant} initial="initial" animate="animate"
+          >
             <img src={seasonData.images.webp.large_image_url} alt="season 1 thumbnail" className="w-full h-full group-hover:scale-105 transition-transform duration-300 ease-in-out" />
             <h1 className="text-xl font-serif mt-10">Season {seasonNumber}</h1>
-          </button>
+          </motion.button>
         ) : (
           <div className="bg-gray-500  bg-gradient-to-b from-slate-900 via-gray-700 to-slate-900 animate-pulse" />
         )
