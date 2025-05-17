6aa00792c51b7e5900d13bd7e37f5366f19d94d1
dot-enny <olaniyaneniola0225@gmail.com>
2025-05-17 16:21:06 +0100

    fix: last episode's thumbnail not displaying.

--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -12,19 +12,20 @@ export default function Episodes() {
     return (
         <>
             <div className={`min-h-screen ${pointerEvents ? 'pointer-events-none' : 'pointer-events-auto'}`}>
-                {animeData && (
+                {animeData && animeEpisodeThumbNails && (
+                    // GRID CONTAINER
                     <motion.div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-20 pb-10 px-14 gap-20"
                         variants={EpisodeGridVariants}
                         initial="initial"
                         animate="animate"
                     >
-                        {animeData.map((episode: any) => (
+                        {/* MAPPED EPISODE CARDS */}
+                        {animeData.map((episode: any, index: number) => (
                             <motion.div className="flex"
                                 key={episode.mal_id}
                                 variants={EpisodeVariants}
                             >
-                                <Episode episodeId={episode.mal_id} image={animeEpisodeThumbNails && animeEpisodeThumbNails[episode.mal_id]?.images.jpg.image_url} setPointerEvents={togglePointerEvents} seasonId={parsedSeasonId} />
-                                {/* {console.log(`Episode ID: ${episode.mal_id}, Image URL: ${animeEpisodeThumbNails && animeEpisodeThumbNails[episode.mal_id]?.images.jpg.image_url}`)} */}
+                                <Episode episodeId={episode.mal_id} image={animeEpisodeThumbNails[index]?.images.jpg.image_url} setPointerEvents={togglePointerEvents} seasonId={parsedSeasonId} />
                             </motion.div>
                         ))}
                     </motion.div>
