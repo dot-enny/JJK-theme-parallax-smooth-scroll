1351f69dcf3343408cabddca4fb7d9b566addfee
dot-enny <olaniyaneniola0225@gmail.com>
2025-05-17 16:21:25 +0100

    fix: ui tweak.

--- src/components/episodes/episode-details/DetailItem.tsx
+++ src/components/episodes/episode-details/DetailItem.tsx
@@ -7,12 +7,12 @@ interface DetailItemProps {
 export const DetailItem = ({ label, value, link }: DetailItemProps) => {
     return (
         <div className="flex flex-wrap max-lg:gap-x-4 lg:grid grid-cols-4">
-            <span className="max-lg:text-nowrap line-clamp-1">{label}:</span>
+            <span className="max-lg:text-nowrap">{label}:</span>
             { link ?
                 ( 
                     <>
-                        <a className="col-span-3 pointer-events-auto max-md:hidden underline" href={value} target="blank">{value.slice(0, 40)}...</a>
-                        <a className="col-span-3 pointer-events-auto md:hidden underline" href={value} target="blank">{value.slice(0, 20)}...</a>
+                        <a className="col-span-3 pointer-events-auto max-md:hidden underline line-clamp-1" href={value} target="blank">{value.slice(0, 40)}...</a>
+                        <a className="col-span-3 pointer-events-auto md:hidden underline line-clamp-1" href={value} target="blank">{value.slice(0, 20)}...</a>
                     </>
                 ) : (
                     <span className="col-span-3">{value}</span>
