6441df34934d49ad1fb5df17ba4671de6500eb40
dot-enny <olaniyaneniola0225@gmail.com>
2025-05-19 18:27:22 +0100

    refactor: moved preloader variants into separate file.

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -2,23 +2,9 @@ import { AnimatePresence, motion } from "framer-motion";
 
 import Logo from "../../public/jjk-logo.svg";
 import { useState } from "react";
-import { easings } from "../utils/animations/animations";
+import { logoVariants, PreloaderVariants } from "../utils/animations/variants/Preloader";
 
 export const Preloader = () => {
-
-  const PreloaderVariants = {
-    exit: { y: "-100%", transition: { duration: 1.5, delay: 0.5, ease: easings.easeOutQuart } }
-  };
-
-  const logoVariants = {
-    initial: { opacity: 0, scale: 0.3 },
-    animate: { 
-      opacity: 1, 
-      scale: 0.5,
-      transition: { duration: 2, ease: easings.easeInOutQuint }
-    },
-    exit: { opacity: 0, scale: 0, transition: { duration: 0.5, ease: 'easeOut' } }
-  };
 
   const [isLogoVisible, setIsLogoVisible] = useState(true);
 
